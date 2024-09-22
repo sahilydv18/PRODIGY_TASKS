ac93d541408ace0c32f5f01024c17b849eed6f58
Sahil Yadav <raosahil15266@gmail.com>
2024-09-22 11:27:14 +0530

    task 2

---
+++ PRODIGY_AD_02/app/src/main/java/com/example/todoapp/db/TodoDatabase.kt
@@ -0,0 +1,21 @@
+package com.example.todoapp.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.todoapp.Todo
+import com.example.todoapp.db.Converters
+import com.example.todoapp.db.TodoDao
+
+
+@Database(entities = [Todo::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class TodoDatabase : RoomDatabase(){
+
+    companion object {
+        const val NAME = "Todo_DB"
+    }
+
+    abstract fun getTodoDao() : TodoDao
+
+}
